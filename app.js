d80ec5441411ccb137cdb9e6cce1d62dc01f6e48
raulmax <raulmax319@gmail.com>
2020-12-27 22:01:21 -0300

    removed comment on app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict;'
+
 import express from 'express';
+import joi from 'joi';
+import bodyParser from 'body-parser';
+import 'dotenv/config.js';
 
 const app = express();
 
 
 app.get('/', (req, res) => {
-    //...
     res.send("Funcionando");
 });
 
